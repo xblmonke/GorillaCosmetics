cad69fc3fcd6ee23be6d8ba3fefd920b04cfdaf6
RedBrumbler <51795403+RedBrumbler@users.noreply.github.com>
2021-02-28 03:42:32 +0100

    Added some files

---
+++ buildQmod.ps1
@@ -0,0 +1,16 @@
+# Builds a .zip file for loading with BMBF
+$NDKPath = Get-Content $PSScriptRoot/ndkpath.txt
+
+$buildScript = "$NDKPath/build/ndk-build"
+if (-not ($PSVersionTable.PSEdition -eq "Core")) {
+    $buildScript += ".cmd"
+}
+
+& $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk -j 4
+Compress-Archive -Path  "./libs/arm64-v8a/libbeatsaber-hook_1_0_12.so",`
+                        "./libs/arm64-v8a/libcustom-types.so",`
+                        "./libs/arm64-v8a/libgorillacosmetics.so",`
+                        "./ExtraFiles/default.json",`
+                        "./mod.json" -DestinationPath "./GorillaCosmetics.zip" -Update
+
+& copy-item -Force "./GorillaCosmetics.zip" "./GorillaCosmetics.qmod"
